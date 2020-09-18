f93b1b434cf62af4dfa5afbef17746bfbe73050d
YBK <ybkamaleri@users.noreply.github.com>
2020-09-18 11:28:02 +0200

    Update package list

--- ins_pkg.R
+++
@@ -1,8 +0,0 @@
-## Required packages
-pkgs <- c("data.table", "epitools")
-
-## Sjekke hvis finnes og installere hvis ikke finnes
-nypkg <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
-if (length(nypkg)) install.packages(nypkg)
-
-sapply(pkgs, require, character.only = TRUE)
--- pkg-install.R
+++ pkg-install.R
@@ -1,4 +1,6 @@
-## install packages to the kh_renv
+## -------------------------------
+## install packages with renv 
+## --------------------------------
 pkgs <- c("RODBC",
           "sas7bdat",
           "XML",
@@ -7,6 +9,30 @@ pkgs <- c("RODBC",
           "sqldf",
           "stringr",
           "intervals",
-          "readxl")
+          "readxl",
+          "assertthat",
+          "backports",
+          "BH",
+          "bit",
+          "bit64",
+          "blob",
+          "cellranger",
+          "chron",
+          "cli")
 
 sapply(pkgs, renv::install)
+
+
+
+## ------------------------
+## Regular install packages
+## ------------------------
+
+## Required packages
+pkgs <- c("data.table", "epitools")
+
+## Sjekke hvis finnes og installere hvis ikke finnes
+nypkg <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
+if (length(nypkg)) install.packages(nypkg)
+
+sapply(pkgs, require, character.only = TRUE)
