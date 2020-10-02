b10f47c0094d4c2818f457984a311184076800c1
Yusman Kamaleri <ybkamaleri@users.noreply.github.com>
2020-10-02 12:15:29 +0200

    Check odbc driver

--- test/test_database.R
+++ test/test_database.R
@@ -24,38 +24,61 @@ dbtest(TRUE)
 
 ## Check ODBC
 odbc::odbcListDrivers()
-
+sort(unique(odbcListDrivers()[[1]]))
 
 
 ## TEST
 library(RODBC)
 ## Set up driver info and database path
-DRIVERINFO <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+DRIVERINFO <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="
+
+## accdb file 
 MDBPATH <- "C:\\enc\\DBtest\\STYRING\\KHELSA_dev2.accdb"
-PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
+PATH <- paste0(DRIVERINFO, MDBPATH)
+
+## mdb file
+MDBPATH <- "C:\\enc\\DBtest\\STYRING\\KHELSA_dev2.mdb"
+PATH <- paste0(DRIVERINFO, MDBPATH)
 
 ## Establish connection
-channel <- odbcDriverConnect(PATH)
+channel <- RODBC::odbcDriverConnect(PATH)
 
 ## Load data into R dataframe
-df <- sqlQuery(channel,
-"SELECT [student_id], [first_name], [last_name],
-FROM [tbl-students]
-ORDER BY [first_name];",
-stringsAsFactors = FALSE)
+RODBC::sqlQuery(channel, "SELECT TOP 5 * FROM KH_DELER")
+RODBC::sqlFetch(channel, "KH_DELER", max = 5)
 
 ## Close and remove channel
 close(channel)
 rm(channel)
 
 
+
+
+
+
+
+library("DBI")
 ## With odbc and DBI
 pkg <- c("odbc", "DBI")
 sapply(pkg, require, character.only = TRUE)
 
-db_con <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq="
+db_con <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ="
+
+## accdb file 
 MDBPATH <- "C:\\enc\\DBtest\\STYRING\\KHELSA_dev2.accdb"
+
+## mdb file
 MDBPATH <- "C:\\enc\\DBtest\\STYRING\\KHELSA_dev.mdb"
 
+MDBPATH
 cs <- paste0(db_con, MDBPATH)
 con <- dbConnect(odbc::odbc(), .connection_string = cs)
+
+DBI::dbGetQuery(con, "SELECT TOP 5 * FROM KH_DELER")
+
+
+## Connection Strings
+MDBPATH
+con <- DBI::dbConnect(odbc::odbc(),
+                      driver = "Microsoft Access Driver (*.mdb, *.accdb)",
+                      database = MDBPATH)
